6dcfe2e2b8907850605b9bd4020b0878db50d177
wiken <wiken@hehuapei.com>
2026-05-28 20:04:54 +0800

    Change keyword VARCHAR length from 255 to 191

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS `count` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `keyword` VARCHAR(255) NOT NULL,
-  `total` BIGINT NOT NULL,
-  `create_time` BIGINT DEFAULT NULL,
-  `update_time` BIGINT DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `keyword` (`keyword`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+ CREATE TABLE IF NOT EXISTS `count` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `keyword` VARCHAR(191) NOT NULL,
+    `total` BIGINT NOT NULL,
+    `create_time` BIGINT DEFAULT NULL,
+    `update_time` BIGINT DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `keyword` (`keyword`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
